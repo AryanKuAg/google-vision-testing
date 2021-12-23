403fa83f812c76925597777e8ebd5844da374f45
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-12-23 22:45:14 +0530

    name part done

--- columnWiseToList.py
+++ columnWiseToList.py
@@ -1,13 +1,23 @@
 
-
+import re
 from typing import final
 
 
 def columnWiseToList(rawData):
     finalColumnSeperatedList = []
 
-    # taking data tracker
-    takeData = True
+    # Date pattern
+    pattern1 = re.compile("\d\d-\w\w\w-\d\d")  # 33-Mar-43
+    pattern2 = re.compile("\d-\w\w\w-\d\d")  # 5-Jul-34
+    pattern3 = re.compile("\d-\w\w\w\d")  # 6-Mar6
+    pattern4 = re.compile("\d-\w\w\w-\d")  # 4-mar-3
+    pattern5 = re.compile("\d\w\w\w-\d")  # 6mar-3
+    pattern6 = re.compile("-\w\w\w-\d")  # -Dec-06
+    pattern7 = re.compile("\d\d-\w\w\w- \d\d")  # 10-May- 12
+    pattern8 = re.compile("\d\d\w\w\w-\d\d")  # 24Aug-07
+    pattern9 = re.compile("\d-\w\w-\d")  # 4-Ap-0?
+    pattern10 = re.compile("\d\d-\w\w\w-\d")  # 13-Apr-0?
+    # pattern11 = re.compile("\w\w\w-\d\d")  # May-84
 
     for index, element in enumerate(rawData):
 
@@ -41,5 +51,22 @@ def columnWiseToList(rawData):
 
         #################################################################
         ##########EVERYTHING IS WELL AND GOOD ABOVE######################
+        #################################################################
+
+        # This is for the name or customer name
+
+        folioNumberIndex = element.index(
+            finalColumnSeperatedList[index][2][0])
+        # This is just initial data if we don't able to find it okay...)_)
+        nameLastIndex = element.index(
+            finalColumnSeperatedList[index][2][0]) + 10
+        for yoyo in range(folioNumberIndex, folioNumberIndex + 30):
+            if '-' in element[yoyo]:
+                nameLastIndex = yoyo - 2
+                break
+
+        #print('name', str(element[folioNumberIndex + 6:nameLastIndex]).strip())
+        finalColumnSeperatedList[index][3].append(
+            str(element[folioNumberIndex + 6:nameLastIndex]).strip())
 
     print(finalColumnSeperatedList)
