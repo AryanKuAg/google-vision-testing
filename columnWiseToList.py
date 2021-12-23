7522b8aad6c00c9be684c62211237156bb29ef22
Aryan Agrawal <alemantrixaryan@gmail.com>
2021-12-23 21:56:21 +0530

    folio number added

--- columnWiseToList.py
+++ columnWiseToList.py
@@ -15,7 +15,7 @@ def columnWiseToList(rawData):
             [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], ])
         splitted = element.split()
 
-        # Initial date of contact and ledger number
+        # Initial date of contact
         if len(splitted[0]) < 10:
 
             finalColumnSeperatedList[index][0].append(splitted[0])
@@ -25,3 +25,21 @@ def columnWiseToList(rawData):
                 splitted[0][0:len(splitted[0]) - 4])
             finalColumnSeperatedList[index][1].append(
                 splitted[0][len(splitted[0]) - 4:])
+
+        # Ledger number
+        if len(finalColumnSeperatedList[index][1]) == 0 and len(splitted[1]) < 6:
+            finalColumnSeperatedList[index][1].append(
+                splitted[1])
+
+        # Folio Number
+        if len(splitted[1]) == 6:
+            finalColumnSeperatedList[index][2].append(
+                splitted[1])
+        elif len(splitted[2]) == 6:
+            finalColumnSeperatedList[index][2].append(
+                splitted[2])
+
+        #################################################################
+        ##########EVERYTHING IS WELL AND GOOD ABOVE######################
+
+    print(finalColumnSeperatedList)
